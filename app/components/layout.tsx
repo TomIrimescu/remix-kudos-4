8d378d5acd8ae90e6649f5b9097284518c0e8150
Tom Irimescu <tom@irimescu.com>
2023-07-20 16:19:32 -0400

    Made entire layout more responsive

--- app/components/layout.tsx
+++ app/components/layout.tsx
@@ -1,7 +1,3 @@
 export function Layout({ children }: { children: React.ReactNode }) {
-    return (
-        <div className="h-screen w-full bg-blue-600 font-mono">
-            {children}
-        </div>
-    )
+  return <div className='w-full bg-blue-600 font-mono'>{children}</div>;
 }
