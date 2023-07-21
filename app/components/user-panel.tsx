d6340717f077fc51883de90865e45543077328d1
Tom Irimescu <tom@irimescu.com>
2023-07-21 08:44:14 -0400

    Tweak sign out button

--- app/components/user-panel.tsx
+++ app/components/user-panel.tsx
@@ -27,7 +27,7 @@ export function UserPanel({ users }: props) {
         <form action='/logout' method='post'>
           <button
             type='submit'
-            className='rounded-xl bg-yellow-300 font-semibold text-blue-600 px-3 py-2 transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1'
+            className='rounded-xl bg-yellow-300 font-semibold text-blue-600 px-2.5 py-2.5 transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1'
           >
             Sign Out
           </button>
